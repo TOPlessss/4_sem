62131ff7853968a384209eb6cf1c72b3d1ddb42f
Leshka <31736716+TOPlessss@users.noreply.github.com>
2019-03-22 20:04:08 +0300

    AndroidRPP

---
+++ Laba1/app/src/main/java/com/example/lab1/MainActivity.java
@@ -0,0 +1,78 @@
+package com.example.lab1;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.ViewFlipper;
+
+public class MainActivity extends AppCompatActivity {
+
+    ViewFlipper v_flip;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        int images[] = {R.drawable.left, R.drawable.mid, R.drawable.right, R.drawable.mid};
+
+        v_flip = findViewById(R.id.v_flip);
+
+
+        for (int image: images)
+        {
+            flipperImages(image);
+        }
+
+        start();
+    }
+
+    public void start()
+    {
+        Thread thread = new Thread()
+        {
+            public void run()
+            {
+                try
+                {
+                    sleep(3000);
+                    startActivity(new Intent(MainActivity.this, SecondActivity.class));
+                    finish();
+                }
+                catch (Exception e)
+                {
+                    e.printStackTrace();
+                }
+            }
+        };
+        thread.start();
+    }
+
+//    public void start()
+//    {
+//        new Handler().postDelayed(new Runnable()
+//        {
+//            @Override
+//            public void run()
+//            {
+//                if(!isChangingConfigurations())
+//                {
+//                    startActivity(new Intent(MainActivity.this, SecondActivity.class));
+//                    finish();
+//                }
+//            }
+//        }, 3000);
+//    }
+
+    public void  flipperImages(int image)
+    {
+        ImageView imageView = new ImageView(this);
+        imageView.setBackgroundResource(image);
+
+        v_flip.addView(imageView);
+        v_flip.setFlipInterval(250);
+        v_flip.setAutoStart(true);
+    }
+}
