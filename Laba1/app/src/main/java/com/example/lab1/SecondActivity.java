0af7ab8bcf4e4e76aa7f8519d91dbdb4d91426a0
Leshka <31736716+TOPlessss@users.noreply.github.com>
2019-03-23 23:32:21 +0300

    Lab1

--- Laba1/app/src/main/java/com/example/lab1/SecondActivity.java
+++ Laba1/app/src/main/java/com/example/lab1/SecondActivity.java
@@ -1,13 +1,41 @@
 package com.example.lab1;
 
+import android.content.Context;
+import android.nfc.Tag;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class SecondActivity extends AppCompatActivity {
 
+    private static final String TAG = "SecondActivity";
+
+    private final int size = 100000;
+
+    ArrayList<CountList> list = new ArrayList<>();
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        Log.d(TAG, "onCreate: Started.");
+        ListView mListView = (ListView) findViewById(R.id.listView);
+        arrFill();
+        MyListAdapter adapter = new MyListAdapter(this, R.layout.adapter_view_layout, list);
+        mListView.setAdapter(adapter);
+    }
+
+
+    public void arrFill()
+    {
+        for(int i = 1; i <= size; i++)
+        {
+            CountList count = new CountList(i);
+            list.add(count);
+        }
     }
 }
